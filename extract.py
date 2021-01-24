1db9d59a01cd1ae6682b3fa9397754bf1cf7a632
AnnaMelekhova <anna.melekhova@protonmail.com>
2021-01-24 22:53:09 +0100

    fix extraction

--- extract.py
+++ extract.py
@@ -6,35 +6,41 @@ Created on Tue Dec 29 12:02:37 2020
 """
 
 import json
-import pandas as pd
 
 f=open('train_features_0.jsonl')
 my_lines = f.readlines()
 
 label=[]
-entropy=[]
-has_debug=[]
-has_relocations=[]
-has_signature=[]
-has_resources=[]
-has_tls=[]
-exports=[]
-imports=[]
-imports_l=[]
+inputs=[]
 
 for i in range(0,len(my_lines)):
     lines_dict=json.loads(my_lines[i]) #returns a dictionary
-    label.append(lines_dict['label']) #label list
-    has_debug.append(lines_dict['general']['has_debug']) #has_debug list
-    has_signature.append(lines_dict['general']['has_signature'])
-    has_relocations.append(lines_dict['general']['has_relocations'])
-    has_resources.append(lines_dict['general']['has_resources'])
-    has_tls.append(lines_dict['general']['has_tls'])
-    exports.append(lines_dict['general']['exports'])
-    imports.append(lines_dict['general']['imports'])
-    imports_l.append(lines_dict['imports'])
+    
+    if lines_dict['label'] == 0 or lines_dict[label] == 1:
+        label.append(lines_dict['label'])
+    
+    new_input=[]
+    new_input.append(lines_dict["general"]["has_debug"])
+    new_input.append(lines_dict["general"]["has_tls"])
+    new_input.append(lines_dict["general"]["has_relocations"])
+    new_input.append(lines_dict["general"]["has_signature"])
+    new_input.append(lines_dict['general']['has_resources'])
+    
+    if lines_dict['general']['imports']>0:#if has imports is 1 if not is 0
+        new_input.append(1)
+    else:
+        new_input.append(0)
+        
+    if lines_dict['general']['exports']>0:#if has exports is 1 if not is 0
+        new_input.append(1)
+    else:
+        new_input.append(0)
+
     
     for section in lines_dict['section']['sections']:
-        entropy.append(section['entropy']/8) #entropy list in section
-        
+        new_input.append(section['entropy'])
+        break #extract entropy from first section 
+                    
+    inputs.append(new_input)
+
 
